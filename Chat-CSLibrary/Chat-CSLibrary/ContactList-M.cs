f45b4dedad240866c27d1a5a20ff94a047013599
Hunter Haufler <haufhun@gmail.com>
2017-04-18 22:17:55 -0500

    Added class library for client and server to use and put interfaces inside and contact list implementation.

---
+++ Chat-CSLibrary/Chat-CSLibrary/ContactList-M.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_CSLibrary
+{
+    public class ContactList : IContactList
+    {
+        public List<IContact> Contacts { get; set; }
+        public void AddContact(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveContact(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
