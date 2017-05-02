e08e0125ef45c18c8cf01b3a3b0cad4c9ee49029
Calvin Bauer <nivlac12@gmail.com>
2017-05-02 18:22:40 -0500

    Commented TextMessage

--- Server/Server/Model/TextMessage-M.cs
+++ Server/Server/Model/TextMessage-M.cs
@@ -11,20 +11,29 @@ namespace Server.Model
     [JsonObject(MemberSerialization.OptIn)]
     public class TextMessage : ITextMessage
     {
+        /// <summary>
+        /// Getter for the Body of the message
+        /// </summary>
         [JsonProperty]
         public string Body { get; }
 
+        /// <summary>
+        /// Getter for the Sender of the message
+        /// </summary>
         [JsonProperty]
         public IContact Sender { get; }
 
+        /// <summary>
+        /// Gets the time of the message
+        /// </summary>
         [JsonProperty]
         public DateTime Time { get; } 
 
         /// <summary>
         /// Use this when constructing a new TextMessage object.
         /// </summary>
-        /// <param name="body"></param>
-        /// <param name="sender"></param>
+        /// <param name="body">Body of the message</param>
+        /// <param name="sender">Contact sending the message</param>
         public TextMessage(string body, IContact sender)
         {
             Body = body;
@@ -46,6 +55,10 @@ namespace Server.Model
             Time = time;
         }
 
+        /// <summary>
+        /// The time of the message being sent
+        /// </summary>
+        /// <returns>Format changing of the time</returns>
         public override string ToString()
         {
             return Time.ToLocalTime().ToString() + " " + Sender.Username + ":" + "\n" + Body;
