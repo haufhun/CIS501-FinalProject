f5d10dc1f7b995f4b2396c12404fc4be6240f673
Amber Schubert <amber39@ksu.edu>
2017-05-02 20:26:23 -0500

    ChatDB commenting

--- Client/Client/Model/ChatDB.cs
+++ Client/Client/Model/ChatDB.cs
@@ -6,25 +6,28 @@ using System.Threading.Tasks;
 using Chat_CSLibrary;
 namespace Client.Model
 {
+    /// <summary>
+    /// This is the class in the Model for ChatDB
+    /// </summary>
     public class ChatDB
     {
         /// <summary>
-        /// 
+        /// Getter and setter for user
         /// </summary>
         public User User { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets the Dictionary storing the chatrooms
         /// </summary>
         public Dictionary<string, ChatRoom> ChatRooms { get;}
 
         /// <summary>
-        /// 
+        /// Gets the Dictionary storing the chatroom we are on currently
         /// </summary>
         public Dictionary<string,ChatForm> CurrentChatForm { get;}
         
         /// <summary>
-        /// 
+        /// Initializes the Chat Database
         /// </summary>
         public ChatDB()
         {
